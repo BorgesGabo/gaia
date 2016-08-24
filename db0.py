dc4006e748e7fc5f0b1ed953dc09c0b77969bec0
BorgesGabo <herbertvargas@gmail.com>
2016-08-24 01:12:34 -0500

    Update db0.py

--- db0.py
+++ db0.py
@@ -22,6 +22,7 @@ db.define_table(
 db.define_table(
     'product',
     Field('product_name'),
+    Field('woo_ref'),
     Field('presentation'),
     Field('pres'),
     Field('unit'),
@@ -30,6 +31,14 @@ db.define_table(
     Field('status'),
     format='%(product_name)s')
 
+db.define_table(
+    'po_detail',
+    Field('po_number', db.po),
+    Field('product_name', db.product),
+    Field('po_product_name'),
+    Field('quantity'),
+    Field('po_sku'),
+    )
 
 
 #------------------------------
@@ -59,10 +68,10 @@ db.po.date.requires = IS_DATETIME(format=T('%Y-%m-%d %H:%M:%S'),error_message='m
 # product's table constraints
 #------------------------------
 #using regex expression to validate just one o more words 
-db.product.product_name.requires = IS_MATCH('^\w+( \w+)*$', error_message='Introduce a valid name just alphabets')
+db.product.product_name.requires = IS_MATCH('^\w.+( \w.+)*$', error_message='Introduce a valid name just alphabets')
 db.product.presentation.requires = IS_NOT_EMPTY(error_message='copy here what is on product presentation in woocommerce' )
 db.product.pres.requires = [IS_NOT_EMPTY(error_message='add a quantity' ),IS_MATCH('^[0-9]*$', error_message='Introduce a valid document just numbers')]
-db.product.unit.requires =[ IS_IN_SET(['g','un'], error_message='must be g or un')]
+db.product.unit.requires =[ IS_IN_SET(['g','un', 'ml'], error_message='must be g or un')]
 db.product.sku.requires = [IS_NOT_IN_DB(db, db.product.sku),IS_MATCH('^[0-9]*$', error_message='Introduce a valid document just numbers')]
 db.product.categoria.requires =[ IS_IN_SET(['aceites y aderezos','bebidas','condimentos y especias', 'frutas', 'frutos secos', 'harinas hojuelas y pastas', 'hortalizas', 'huevos','nueces y semillas','pa picar y endulzar','panaderia','proteina de  origen vegetal'], error_message='must be one of the existing categories')]
 db.product.status.requires =[ IS_IN_SET(['disponible','descontinuado','agotado'], error_message='must be disponible, descontinuado o agotado')]
