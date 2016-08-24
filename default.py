676d0369180ac13c95b114c9b1bd27dfff5347b7
BorgesGabo <herbertvargas@gmail.com>
2016-08-24 01:12:57 -0500

    Update default.py

--- default.py
+++ default.py
@@ -38,6 +38,15 @@ def form5():
        response.flash = 'please fill out the form'
    return dict(form=form)
 
+def form6():
+   form = SQLFORM(db.po_detail)
+   if form.process().accepted:
+       response.flash = 'form accepted'
+   elif form.errors:
+       response.flash = 'form has errors'
+   else:
+       response.flash = 'please fill out the form'
+   return dict(form=form)
 
 def index():
     """
