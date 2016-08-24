ac11287a0f11c679b6eb9d8c4a6122b9783007b3
BorgesGabo <herbertvargas@gmail.com>
2016-08-23 21:26:22 -0500

    Update db0.py

--- db0.py
+++ db0.py
@@ -14,3 +14,19 @@ db.define_table(
     Field('phone')
     )
 
+
+#------------------------------
+#Customer's table constraints
+#------------------------------
+
+#using a regex expression validates the just numbers
+db.customer.document.requires = [IS_NOT_IN_DB(db, db.customer.document),IS_MATCH('^[0-9]*$', error_message='Introduce a valid document just numbers')]
+
+#using a regex expression validates words only
+db.customer.full_name.requires = [IS_NOT_EMPTY(),IS_MATCH('^\w+( \w+)*$', error_message='Introduce a valid name just alphabets')]
+db.customer.user_mail.requires = IS_EMAIL()
+
+#using a regex expression validates the phone number format
+# see more at... "http://stackoverflow.com/questions/16699007/regular-expression-to-match-standard-10-digit-phone-number"
+db.customer.phone.requires = IS_MATCH('^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})?[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$',
+         error_message='not a phone number. Introduce a valid number neither spaces nor dots')
