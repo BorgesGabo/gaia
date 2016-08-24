8d946e0aeae7f2619bda4343e0200cb32c317a99
BorgesGabo <herbertvargas@gmail.com>
2016-08-23 22:44:38 -0500

    Update db0.py

--- db0.py
+++ db0.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #defining tables
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
@@ -11,8 +9,17 @@ db.define_table(
     Field('doc'),
     Field('full_name'),
     Field('user_mail'),
-    Field('phone')
-    )
+    Field('phone'),
+    format='%(full_name)s')
+
+db.define_table(
+    'po',
+    Field('po_number'),
+    Field('date', type='datetime'),
+    Field('customer', db.customer),
+    format='%(po_number)s')
+
+
 
 
 #------------------------------
@@ -30,3 +37,11 @@ db.customer.user_mail.requires = IS_EMAIL()
 # see more at... "http://stackoverflow.com/questions/16699007/regular-expression-to-match-standard-10-digit-phone-number"
 db.customer.phone.requires = IS_MATCH('^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})?[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$',
          error_message='not a phone number. Introduce a valid number neither spaces nor dots')
+
+#-----------------------------
+#po's table constraints
+#-----------------------------
+
+db.po.po_number.requires=[IS_NOT_EMPTY(),IS_MATCH('^[0-9]*$', error_message='Introduce a valid document just numbers'), IS_NOT_IN_DB(db,db.po.po_number)]
+db.po.customer.requires=[IS_NOT_EMPTY(),IS_MATCH('^[0-9]*$', error_message='Introduce a valid document just numbers'),IS_IN_DB(db,'customer.id','%(full_name)s')]
+db.po.date.requires = IS_DATETIME(format=T('%Y-%m-%d %H:%M:%S'),error_message='must be YYYY-MM-DD HH:MM:SS!')
