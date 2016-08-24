7adb0461011d4fe4fd1d38da4289c8d9cdcecb04
BorgesGabo <herbertvargas@gmail.com>
2016-08-24 00:34:41 -0500

    Update db0.py

--- db0.py
+++ db0.py
@@ -19,6 +19,16 @@ db.define_table(
     Field('customer', db.customer),
     format='%(po_number)s')
 
+db.define_table(
+    'product',
+    Field('product_name'),
+    Field('presentation'),
+    Field('pres'),
+    Field('unit'),
+    Field('sku'),
+    Field('categoria'),
+    Field('status'),
+    format='%(product_name)s')
 
 
 
@@ -44,3 +54,15 @@ db.customer.phone.requires = IS_MATCH('^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})?[-. )
 
 db.po.po_number.requires=[IS_NOT_EMPTY(),IS_MATCH('^[0-9]*$', error_message='Introduce a valid document just numbers'), IS_NOT_IN_DB(db,db.po.po_number)]
 db.po.date.requires = IS_DATETIME(format=T('%Y-%m-%d %H:%M:%S'),error_message='must be YYYY-MM-DD HH:MM:SS!')
+
+#------------------------------
+# product's table constraints
+#------------------------------
+#using regex expression to validate just one o more words 
+db.product.product_name.requires = IS_MATCH('^\w+( \w+)*$', error_message='Introduce a valid name just alphabets')
+db.product.presentation.requires = IS_NOT_EMPTY(error_message='copy here what is on product presentation in woocommerce' )
+db.product.pres.requires = [IS_NOT_EMPTY(error_message='add a quantity' ),IS_MATCH('^[0-9]*$', error_message='Introduce a valid document just numbers')]
+db.product.unit.requires =[ IS_IN_SET(['g','un'], error_message='must be g or un')]
+db.product.sku.requires = [IS_NOT_IN_DB(db, db.product.sku),IS_MATCH('^[0-9]*$', error_message='Introduce a valid document just numbers')]
+db.product.categoria.requires =[ IS_IN_SET(['aceites y aderezos','bebidas','condimentos y especias', 'frutas', 'frutos secos', 'harinas hojuelas y pastas', 'hortalizas', 'huevos','nueces y semillas','pa picar y endulzar','panaderia','proteina de  origen vegetal'], error_message='must be one of the existing categories')]
+db.product.status.requires =[ IS_IN_SET(['disponible','descontinuado','agotado'], error_message='must be disponible, descontinuado o agotado')]
