9e5ced9f91aceb9f4a5c1bde48d1a826e415ae17
BorgesGabo <herbertvargas@gmail.com>
2016-10-15 21:13:52 -0500

    Create B.py

---
+++ B.py
@@ -0,0 +1,20 @@
+def B:
+
+query...
+
+#filtra los del primer producto de A -> DAL> query_A
+query_A= query
+query_A &= A[0]['id']
+
+#obtiene todos los pedidos-> lista> pedidos_lst, cuenta No pedidos -> int> n
+pedidos_lst=db(query_A).select(db.po.id orderby=db.po.po_number).as_list()
+
+#for e/a pedido busca po_detail.quantity(j) si es vacio ponga '0' else product.pres(j)*po_detail.quantity(j) ->B
+#cada loop append-> B
+
+query_B=query
+query_B=db().select()
+for j in range (0, n):
+  
+
+return dic()
